0b0f795b2c1de6ac4f82a711482397ef3ce2068f
Nathaniel <ngreen26@terpmail.umd.edu>
2025-05-01 00:18:57 -0400

    maze update

--- src/turtle_move/setup.py
+++ src/turtle_move/setup.py
@@ -19,7 +19,7 @@ setup(
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': ['move = turtle_move.publisher:main', 'read = turtle_move.subscriber:main'
+        'console_scripts': ['move = turtle_move.publisher:main', 'read = turtle_move.subscriber:main', 'test = turtle_move.logic_test:main'
         ],
     },
 )
